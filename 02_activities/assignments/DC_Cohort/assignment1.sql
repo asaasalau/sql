c17e707c1564de31ceab0d27ec01351d3c9a2a97
Asa Lau <asalau711@gmail.com>
2026-04-01 16:12:06 -0400

    SQL Assignment one Upload

--- 02_activities/assignments/DC_Cohort/assignment1.sql
+++ 02_activities/assignments/DC_Cohort/assignment1.sql
@@ -2,32 +2,31 @@
 --Please write responses between the QUERY # and END QUERY blocks
 /* SECTION 2 */
 
-
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 --QUERY 1
-
-
-
+SELECT * FROM customer;
 
 --END QUERY
-
 
 /* 2. Write a query that displays all of the columns and 10 rows from the customer table, 
 sorted by customer_last_name, then customer_first_ name. */
 --QUERY 2
-
-
-
+SELECT * 
+FROM customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 10;
 
 --END QUERY
-
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. 
 Limit to 25 rows of output. */
-
-
+SELECT *
+FROM customer_purchases
+WHERE product_id = 4 
+OR product_id = 9
+LIMIT 25;
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
 filtered by customer IDs between 8 and 10 (inclusive) using either:
@@ -36,9 +35,10 @@ filtered by customer IDs between 8 and 10 (inclusive) using either:
 Limit to 25 rows of output.
 */
 --QUERY 3
-
-
-
+SELECT *, quantity * cost_to_customer_per_qty AS price
+FROM customer_purchases
+WHERE customer_id BETWEEN 8 AND 10
+LIMIT 25;
 
 --END QUERY
 
@@ -50,8 +50,11 @@ columns and add a column called prod_qty_type_condensed that displays the word �
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 --QUERY 4
 
-
-
+SELECT product_id, product_name
+,CASE WHEN product_qty_type = 'unit' THEN 'unit'
+	ELSE 'bulk'
+	END AS prod_qty_type_condensed
+FROM product;
 
 --END QUERY
 
@@ -60,9 +63,14 @@ if the product_qty_type is “unit,” and otherwise displays the word “bulk.�
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 --QUERY 5
-
-
-
+SELECT product_id, product_name
+,CASE WHEN product_qty_type = 'unit' THEN 'unit'
+	ELSE 'bulk'
+	END AS prod_qty_type_condensed
+,CASE WHEN product_name LIKE '%epper%' THEN '1'
+	ELSE '0'
+	END AS pepper_flag
+FROM product;
 
 --END QUERY
 
@@ -72,9 +80,12 @@ contains the word “pepper” (regardless of capitalization), and otherwise out
 vendor_id field they both have in common, and sorts the result by market_date, then vendor_name.
 Limit to 24 rows of output. */
 --QUERY 6
-
-
-
+SELECT *
+FROM vendor_booth_assignments
+INNER JOIN vendor
+	ON vendor_booth_assignments.vendor_id = vendor.vendor_id
+ORDER BY market_date, vendor_name
+LIMIT 24;
 
 --END QUERY
 
@@ -86,9 +97,10 @@ Limit to 24 rows of output. */
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 --QUERY 7
-
-
-
+SELECT vendor_id
+,COUNT(*)
+FROM vendor_booth_assignments
+GROUP BY vendor_id;
 
 --END QUERY
 
@@ -99,9 +111,14 @@ of customers for them to give stickers to, sorted by last name, then first name.
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
 --QUERY 8
-
-
-
+SELECT *
+	,SUM(quantity * cost_to_customer_per_qty) AS total_purchase
+FROM customer_purchases as cp
+LEFT JOIN customer as c
+	ON cp.customer_id = c.customer_id
+GROUP BY c.customer_id
+HAVING total_purchase > 2000
+ORDER BY customer_last_name, customer_first_name
 
 --END QUERY
 
@@ -118,9 +135,14 @@ When inserting the new vendor, you need to appropriately align the columns to be
 VALUES(col1,col2,col3,col4,col5) 
 */
 --QUERY 9
+CREATE TABLE temp.new_vendor AS
+SELECT *
+FROM vendor
 
+INSERT INTO temp.new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+	VALUES (10, 'Thomass Superfood Store', 'a Fresh Focused store', 'Thomas', 'Rosenthal')
 
-
+SELECT * FROM temp.new_vendor
 
 --END QUERY
 
